7e87c2ff9218e12c28f1e8a121befd62c00597f1
Prathik M. <murthypr@umich.edu>
2025-10-26 15:15:57 -0400

    looped tcp connections

--- src/lib.rs
+++ src/lib.rs
@@ -9,27 +9,56 @@ pub mod server {
         let listener = TcpListener::bind("127.0.0.1:34254")?;
         for conn in listener.incoming() {
             let mut sock = conn?;
-            let msg = receive_message(&mut sock)?;
-            println!("Received message: {}", msg);
+            loop {
+                let msg = receive_message(&mut sock)?;
+                if msg.is_empty() {
+                    break;
+                }
+                println!("Received message: {}", msg);
 
-            send_message(&mut sock, "pong")?;
+                match msg.as_str() {
+                    "ping" => {
+                        send_message(&mut sock, "pong")?;
+                    }
+                    "quit" => {
+                        send_message(&mut sock, "")?;
+                        break;
+                    }
+                    other => {
+                        send_message(&mut sock, other)?;
+                    }
+                }
+            }
         }
         Ok(())
     }
 }
 
 pub mod client {
-    use std::net::TcpStream;
+    use std::{net::TcpStream};
+
+    use prompted::input;
 
     use crate::tcp::{receive_message, send_message};
 
     pub fn run() -> std::io::Result<()> {
         let mut stream = TcpStream::connect("127.0.0.1:34254")?;
 
-        send_message(&mut stream, "ping")?;
+        loop {
+            let msg = input!("");
 
-        let response = receive_message(&mut stream)?;
-        println!("Received message: {}", response);
+            send_message(&mut stream, &msg)?;
+    
+            let response = receive_message(&mut stream)?;
+            
+            if response.is_empty() {
+                break;
+            }
+
+            println!("Received message: {}", response);
+            
+        }
+
         Ok(())
     }
 }
